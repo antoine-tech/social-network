056e14f0ef1701377ee87c9d855185d84e0fb72a
antoine.tech <antoinehadrienleguillou@outlook.com>
2020-11-08 12:47:55 +0100

    binding current user internal state of app to cookie presence

--- src/config/API/ApiBaseUrl.js
+++ src/config/API/ApiBaseUrl.js
@@ -1,9 +1,9 @@
 // GLOBAL API 
 
-//const API_BASE_URL = "https://api-social-network-next.herokuapp.com";
+const API_BASE_URL = "https://api-social-network-next.herokuapp.com";
 
 // LOCAL API  
-const API_BASE_URL = "http://localhost:1337";
+//const API_BASE_URL = "http://localhost:1337";
 
 
 export default API_BASE_URL;
